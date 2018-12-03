a4aaa636a8db7be44e08fa149463335441873ffa
kswr <ksawery.oleszynski@gmail.com>
2018-12-03 14:57:05 +0100

    Initial commit

---
+++ src/main/java/com/kswr/wallets/api/walletsapi/WalletsApiApplication.java
@@ -0,0 +1,12 @@
+package com.kswr.wallets.api.walletsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletsApiApplication.class, args);
+    }
+}
